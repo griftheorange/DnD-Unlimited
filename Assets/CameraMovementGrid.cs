5db4e873876d5c466b4382badb04470f9b3da310
Griffin Poole <griffinpoole5@gmail.com>
2020-04-07 13:19:56 -0400

    Settup for Grid based movement, broke animation

---
+++ Assets/CameraMovementGrid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementGrid : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 motion = new Vector3(movePoint.position.x, movePoint.position.y, -10f);
+        transform.position = Vector3.MoveTowards(transform.position, motion, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerMovementGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementGrid : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            animator.SetFloat("Speed", 0);
+            if(Mathf.Abs(horizontal) == 1f)
+            {
+                animator.SetFloat("Horizontal", horizontal);
+                movePoint.position += new Vector3(horizontal, 0f, 0f);
+            } else if(Mathf.Abs(vertical) == 1f)
+            {
+                animator.SetFloat("Vertical", vertical);
+                movePoint.position += new Vector3(0f, vertical, 0f);
+            }
+        } else
+        {
+            animator.SetFloat("Speed", moveSpeed);
+        }
+    }
+}
